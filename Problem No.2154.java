0738aeb366d0959a17dbd84024dbe57401fd7b50
AMRIT KUMAR DAS <amritkumar.das2024@gmail.com>
2025-12-15 09:00:28 +0530

    Implement findFinalValue method in Solution class

---
+++ Problem No.2154.java
@@ -0,0 +1,18 @@
+import java.util.HashSet;
+import java.util.Set;
+
+class Solution {
+    public int findFinalValue(int[] nums, int original) {
+
+        Set<Integer> numSet = new HashSet<>();
+        for (int num : nums) {
+            numSet.add(num);
+        }
+
+        while (numSet.contains(original)) {
+            original <<= 1;
+        }
+
+        return original;
+    }
+}
